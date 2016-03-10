6783308868b8b685e7d255d071eacd921d4879d6
mobilestudio <mobilestudio@mobilestudios-MacBook-Pro.local>
2016-03-09 21:40:38 -0700

    First commit

--- SquirrelHide/ViewController.swift
+++ SquirrelHide/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonRed: UIButton!
+    @IBOutlet weak var buttonBlue: UIButton!
+    @IBOutlet weak var squirrelRed: UIImageView!
+    @IBOutlet weak var squirrelBlue: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideRed(sender: AnyObject) {
+        squirrelRed.hidden = true
+    }
+    
+    @IBAction func hideBlue(sender: AnyObject) {
+        squirrelBlue.hidden = true;
+    }
 
 }
 
